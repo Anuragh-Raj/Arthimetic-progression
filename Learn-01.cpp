7edc42c02d71adf58cd6b23a87388e89e36bd44d
Anuragh-Raj <anikraj.professional@gmail.com>
2025-05-10 11:03:24 +0530

    Learn-01.cpp

--- Learn-01.cpp
+++ Learn-01.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-    float a,d;
-    int n;
-    char input = 'Y';
+  float a,d;
+  int n;
+  char input = 'Y';
     cout<<"Put the value of a: ";
     cin>>a;
     cout<<"Common difference: ";
@@ -11,15 +11,19 @@ int main(){
     cout<<"Upto How many Terms: ";
     cin>>n;
 
-    for(int k=1; (k-1)<=n; k++){
+  for(int k=1; (k-1)<=n; k++){
         cout<<(a+d*(k-1))<<", ";
     }
     cout<<(a+d*(n-1))<<endl;
     cout<<"Do you want the sum of n terms: (Y/N) ";
     cin>>input;
-    if(input == 'Y'){
-        cout<<endl<<"The sum of n terms is: "<<((n/2)*(2*a+(n-1)*d))<<endl;
+    
+  if(input == 'Y'){
+        cout<<endl<<"The sum of n terms is: ";
+        cout<<((n/2)*(2*a+(n-1)*d))<<endl;
     }
+  else{
+        cout<<"Pleasure to help you";
+  }
     return 0;
-
 }
